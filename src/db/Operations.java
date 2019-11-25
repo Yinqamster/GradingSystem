c78abd6b450c7ccaca57e76e2b66825bc089cab3
sunjiaqian1218@163.com <sunjiaqian1218@163.com>
2019-11-25 17:21:35 -0500

    Add DB

--- src/db/Operations.java
+++ src/db/Operations.java
@@ -1,4 +1,36 @@
 package db;
 
+import java.sql.*;
+
 public class Operations {
+    public static void main(String[] args) {
+        Connection con;
+        String driver = "com.mysql.jdbc.Driver";
+        //这里我的数据库是qcl
+        String url = "jdbc:mysql://localhost:3306/GradingSystem";
+        String user = "root";
+        String password = "SJQyingqi1218320";
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            if (!con.isClosed()) {
+                System.out.println("数据库连接成功");
+            }
+            Statement statement = con.createStatement();
+            String sql = "select * from Name;";//我的表格叫home
+            ResultSet resultSet = statement.executeQuery(sql);
+            String name;
+            while (resultSet.next()) {
+                name = resultSet.getString("First Name");
+                System.out.println("姓名：" + name);
+            }
+            resultSet.close();
+            con.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("数据库驱动没有安装");
+
+        } catch (SQLException e) {
+            System.out.println("数据库连接失败");
+        }
+    }
 }
