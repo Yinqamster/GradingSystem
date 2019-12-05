ca4291cf96f60c6795aa3cf17c080336eaaf0635
Qi Yin <msyinqi@outlook.com>
2019-12-05 15:47:33 -0500

    import student from excel add some jars

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,11 +1,16 @@
 package service;
 
 import model.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import utils.Config;
 import utils.ErrCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.*;
 
 public class StudentService {
     private static StudentService instance = new StudentService();
@@ -25,8 +30,54 @@ public class StudentService {
     public Map<String, Student> importStudent(String filename) {
         Map<String, Student> students = new HashMap<>();
         // TODO: read students from worksheet
+        /*the column of the first four cols should be
+         * buid
+         * first name
+         * middle name
+         * last name
+         * and the cols after these cols don't matter.
+         */
+        try {
+            File file = new File(filename);   //creating a new file instance
+            FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheetAt(0);     //creating a Sheet object to retrieve object
+
+            int rolNum = sheet.getLastRowNum();
+            int colNum = sheet.getRow(0).getLastCellNum();
+
+            for(int i = 1; i <= rolNum; i++) {
+                Row row = sheet.getRow(i);
+                List<Object> elements = new ArrayList<>();
+                for(int j = 0; j < colNum; j++) {
+                    Cell cell = row.getCell(j);
+                    if(cell == null) {
+                        elements.add("");
+                    }
+                    else {
+                        elements.add(cell.getStringCellValue());
+                    }
+                }
+                Name name = new Name(elements.get(1).toString(), elements.get(2).toString(), elements.get(3).toString());
+                Student student = new UndergraduateStudent(name, elements.get(0).toString());
+                students.put(student.getBuid(), student);
+            }
+
+            //only for test, can be deleted
+            printStudent(students);
+        } catch(Exception e) {
+            return null;
+        }
         // TODO: add students to DB
         return students;
+    }
+
+    public void printStudent(Map<String, Student> students) {
+        System.out.println("student size: " + students.size());
+        for(Student s : students.values()) {
+            System.out.print(s.getBuid());
+            System.out.println(" " + s.getName().getFullName());
+        }
     }
 
     public int addStudent(String firstname, String midname, String lastname, String buid, String year, String comment, String courseId) {
