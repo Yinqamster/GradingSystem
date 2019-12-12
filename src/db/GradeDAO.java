e054cd7b83d06ed07d50feb7f00cf9399604bed6
QingShuiXiFan <994213053@qq.com>
2019-12-12 00:19:04 -0500

    update

--- src/db/GradeDAO.java
+++ src/db/GradeDAO.java
@@ -78,17 +78,17 @@ public class GradeDAO {
             conn.close();
             return result;
         } catch (SQLException sqle) {
-            return result;
+            return null;
         }
     }
 
-    public int upgradeGrade(String courseId, String buid, Grade grade) {
-        String updateSql = "REPLACE INTO grade (fk_course, fk_student, absolute_score, percentage_score, deduction_score, comment, fk_grading_rule, name)" +
-                "values (?, ?, ?, ?, ?, ?, ?, ?)";
+    public int upgradeGrade(String ruleId, String buid, Grade grade) {
+        String updateSql = "REPLACE INTO grade (fk_grading_rule, fk_student, absolute_score, percentage_score, deduction_score, comment, name)" +
+                "values (?, ?, ?, ?, ?, ?, ?)";
         try {
             Connection conn = DBUtil.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(updateSql);
-            preparedStatement.setObject(1, courseId);
+            preparedStatement.setObject(1, ruleId);
             preparedStatement.setObject(2, buid);
             preparedStatement.setObject(3, grade.getAbsolute());
             preparedStatement.setObject(4, grade.getPercentage());
@@ -158,49 +158,5 @@ public class GradeDAO {
             flag *= deleteGrade(buid, courseId, entry.getValue().getRuleId());
         }
         return flag;
-    }
-
-    public int addGrade(String courseId, String gradingRuldId, String name) {
-        List<String> studentId = StudentDAO.getInstance().getStudentIdByCourseId(courseId);
-        int updateFlag = 1;
-        String updateSql = "REPLACE INTO grade (fk_student, fk_grading_rule, fk_course, name) values (?, ?, ?, ?)";
-        try {
-            for (String id : studentId) {
-                Connection conn = DBUtil.getConnection();
-                PreparedStatement preparedStatement = conn.prepareStatement(updateSql);
-                preparedStatement.setObject(1, id);
-                preparedStatement.setObject(2, gradingRuldId);
-                preparedStatement.setObject(3, courseId);
-                preparedStatement.setObject(4, name);
-                updateFlag *= preparedStatement.executeUpdate();
-            }
-            return updateFlag == 0 ? ErrCode.UPDATEERROR.getCode() : ErrCode.OK.getCode();
-        } catch (SQLException sqle) {
-            return ErrCode.UPDATEERROR.getCode();
-        }
-    }
-
-    public List<List<Object>> getGradeFromGradingRule(String courseId) {
-        String selectSql = "SELECT * FROM grading_rule WHERE fk_breakdown = ?";
-        List<List<Object>> result = new ArrayList<>();
-        try {
-            Connection conn = DBUtil.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(selectSql);
-            preparedStatement.setObject(1, courseId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()) {
-                List<Object> temp = new ArrayList<>();
-                temp.add(resultSet.getString("name"));
-                temp.add(resultSet.getString("grading_rule_id"));
-                temp.add(resultSet.getDouble("full_score"));
-                result.add(temp);
-            }
-            resultSet.close();
-            preparedStatement.close();
-            conn.close();
-            return result;
-        } catch(SQLException sqle) {
-            return result;
-        }
     }
 }
