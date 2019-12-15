426b0a23d6657acd365ee848104ea8f9c867e4c3
Yinqamster <1052317189@qq.com>
2019-12-15 13:49:40 -0500

    fix bug

--- src/service/TemplateService.java
+++ src/service/TemplateService.java
@@ -3,13 +3,11 @@ package service;
 import db.BreakdownDAO;
 import db.TemplateDAO;
 import model.Breakdown;
+import model.GradingRule;
 import model.Template;
 import utils.ErrCode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TemplateService {
 
@@ -46,10 +44,29 @@ public class TemplateService {
         return TemplateDAO.getInstance().getTemplate(templateId);
     }
 
+    public void generateGradingRuleID(GradingRule gradingRule, String parentId){
+        gradingRule.setId(UUID.randomUUID().toString());
+        gradingRule.setParentID(parentId);
+        if(gradingRule.getChildren() != null && gradingRule.getChildren().size() != 0) {
+            for(GradingRule g : gradingRule.getChildren()) {
+                generateGradingRuleID(g, gradingRule.getId());
+            }
+        }
+    }
+
     public int saveTemplate(String courseId, String templateName) {
         Breakdown breakdown = BreakdownDAO.getInstance().getBreakdown(courseId);
+
+        Map<String, GradingRule> gradingRuleMap = breakdown.getGradingRules();
+        Map<String, GradingRule> newGradingRuleMap = new HashMap<>();
+        for(GradingRule gradingRule : gradingRuleMap.values()) {
+            generateGradingRuleID(gradingRule, "");
+            newGradingRuleMap.put(gradingRule.getId(), gradingRule);
+        }
+        breakdown.setGradingRules(newGradingRuleMap);
+
+
         Template template = new Template(breakdown.getBreakdownID(), templateName, breakdown.getGradingRules(), breakdown.getLetterRule());
-//        return TemplateDAO.getInstance().addTemplate(courseId, template);
         return TemplateDAO.getInstance().updateTemplate(template);
     }
 }
